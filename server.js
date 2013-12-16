5d24c447173f2fc1227c924ccc82eb0f865ae57f
wei <cqqsboy@gmail.com>
2013-12-16 22:53:04 +0800

    now all file ok ,but no display as code

--- server.js
+++ server.js
@@ -1,8 +1,12 @@
+/* this is chatrooms */
 var http = require('http');
 var fs = require('fs');
 var path = require('path');
 var mime = require('mime');
 var cache = {};
+
+
+
 
  function send404(response) {
 	response.writeHead(404, {'Content-Type': 'text/plain'});
@@ -53,7 +57,8 @@ var server = http.createServer(function(request, response) {
 server.listen(3000, function() {
 	console.log("Server listening on port 3000.");
 });
-
+var chatServer = require('./lib/chat_server');
+chatServer.listen(server);
 
 
 
